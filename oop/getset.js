a88e3b0983750f8e1a954c00359ce04783347708
DESKTOP-7TS4NVF\kaans <senox57400@gmail.com>
2022-01-05 00:56:06 +0300

    getter setter added

---
+++ oop/getset.js
@@ -0,0 +1,35 @@
+const person = {
+  firstName: "",
+  lastName: "",
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  },
+  set fullName(value) {
+    let data = value.split(" ");
+    this.firstName = data[0];
+    this.lastName = data[1];
+  },
+};
+
+// Object.defineProperty(person, "fullName", {
+//   get function() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+//   set function(value) {
+//     const parts = value.split(" ");
+//     this.firstName = parts[0];
+//     this.lastName = parts[1];
+//   },
+// });
+
+Object.defineProperty(person, "age", {
+  value: 50,
+  writable: false,
+});
+
+person.age = 55;
+
+person.fullName = "Kaan ŞEN";
+console.log(person.age);
+console.log(person.fullName);
+console.log(person);
