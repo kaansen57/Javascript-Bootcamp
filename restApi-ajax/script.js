bea51a18339c8d16e33ba745016bdc89c4d7029b
DESKTOP-7TS4NVF\kaans <senox57400@gmail.com>
2022-01-13 18:08:36 +0300

    ajax json practice

--- restApi-ajax/script.js
+++ restApi-ajax/script.js
@@ -1,20 +1,72 @@
 var xhr = new XMLHttpRequest();
+const spinner = document.querySelector("#spinnerr");
+const getEmployees = document.querySelector("#getEmployees");
+const getEmployee = document.querySelector("#getEmployee");
+const table = document.querySelector("#employeeTable");
+const inputValue = document.querySelector("#firstname");
 
-console.log(xhr);
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      console.log("başarılı");
-      console.log(xhr.responseText);
-    } else if (xhr.status === 0) {
-      console.log("istek atılmadı");
-    } else if (xhr.status === 404) {
-      console.log("hatalı istek");
+function tableReset() {
+  table.replaceChildren();
+}
+getEmployees.addEventListener("click", function () {
+  spinner.className = "spinner-border text-success";
+  xhr.open("GET", "employees.json", true);
+  xhr.send();
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      tableReset();
+      spinner.className = "visually-hidden";
+
+      let data = JSON.parse(this.response);
+        data.employees.forEach((element) => {
+          table.innerHTML += `<tr> 
+        <td> ${element.firstName}</td> 
+        <td> ${element.lastName}</td> 
+        <td> ${element.age}</td>
+            </tr>`;
+        });
+          
+      } else if(this.status === 404) {
+        console.log("sunucu hatası");
+      }
+    } 
+});
+
+getEmployee.addEventListener("click", function () {
+  spinner.className = "spinner-border text-success";
+  xhr.open("GET", "employees.json", true);
+  xhr.send();
+
+  xhr.onload = () => {
+    if (this.status === 200) {
+      tableReset();
+      spinner.className = "visually-hidden";
+      let data = JSON.parse(this.response);
+      let getEmployee = data.employees.filter((x) => {
+        return x.firstName === inputValue.value;
+      });
+
+      getEmployee.forEach((x) => {
+        table.innerHTML += `<tr> 
+        <td> ${x.firstName}</td> 
+        <td> ${x.lastName}</td> 
+        <td> ${x.age}</td>
+         </tr>`;
+      });
+    } else {
+      console.log("sunucu hatası");
     }
-  }
-};
-xhr.onprogress = function () {
-  console.log("yükleniyor lütfen bekleyiniz");
-};
-xhr.open("GET", "msg.txt", true);
-xhr.send();
+  };
+});
+
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4) {
+//     if (xhr.status === 200) {
+//       console.log("başarılı");
+//       console.log(JSON.parse(xhr.response));
+//     } else if (xhr.status === 404) {
+//       console.log("hatalı istek");
+//     }
+//   }
+// }
